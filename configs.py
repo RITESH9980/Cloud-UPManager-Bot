2446ad81e88050ba357efcf8f2f937f32b8b02c2
RITESH9980 <132210046+RITESH9980@users.noreply.github.com>
2023-05-10 07:59:14 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -4,15 +4,15 @@ import os
 
 
 class Config(object):
-	API_ID = int(os.environ.get("API_ID"))
-	API_HASH = os.environ.get("API_HASH")
-	BOT_TOKEN = os.environ.get("BOT_TOKEN")
+	API_ID = int(os.environ.get("21347819"))
+	API_HASH = os.environ.get("3d9e35374cc5aa394c123dd8062b3469")
+	BOT_TOKEN = os.environ.get("6124896722:AAFwfLrJjdISzCcFheCEACyQz-RwdJvAtZ8")
 	GOFILE_API = os.environ.get("GOFILE_API")
-	STREAMTAPE_API_PASS = os.environ.get("STREAMTAPE_API_PASS")
-	STREAMTAPE_API_USERNAME = os.environ.get("STREAMTAPE_API_USERNAME")
+	STREAMTAPE_API_PASS = os.environ.get("bQ9j1GMrrltPkQK")
+	STREAMTAPE_API_USERNAME = os.environ.get("ede3799345194ba16e61")
 	SESSION_NAME = os.environ.get("SESSION_NAME", "CloudManagerBot")
-	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
-	LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL"))
+	BOT_OWNER = int(os.environ.get("27723431"))
+	LOG_CHANNEL = int(os.environ.get("-1001943348198"))
 	DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
 	HELP_TEXT = """
 Send me any Media & Choose Upload Server,
